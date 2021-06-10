58ad80696a48ba8f05a787fe520aa339c6605482
BgitBB <61750923+BgitBB@users.noreply.github.com>
2021-06-10 22:50:25 +0300

    functionality for when a chat is opened

--- src/socketio_tbed.js
+++ src/socketio_tbed.js
@@ -16,6 +16,7 @@ function main(){
     var socket = io('http://localhost:5000');
     // var socket = io.connect(location.protocol + '//' + document.domain + ':' + location.port);
 
+
     //will receive ordered recentMessages list of the form { chat_id:{msg}, chat_id:{msg}, chat_id:{msg} } where chat_id is chatroom_id or contact_id
     socket.on('recentMessages', function(recentmsgs) {
         // socket.emit('event', {data: 'Test'});
@@ -47,14 +48,50 @@ function main(){
 
 
     //------------------------------------create chats---------------------------------//
-    //------------------------------------create rooms/adde contacts---------------------------------//
+    //------------------------------------create rooms/add contacts---------------------------------//
     //------------------------------------join rooms---------------------------------//
+    // let newContactForm, newRoomForm, joinNewRoomForm;
+    // newContactForm.addEventListener('click', createNewContact); //gets username
+    // newRoomForm.addEventListener('click', createNewRoom); //gets roomName, members(select input)
+    // joinNewRoomForm.addEventListener('click', joinNewRoom); //gets room id
+
+    //rgh
+    // socket.emit('joinNewRoom', room_id);
+    // socket.emit('joinNewRoom', contact_id);
+    // ...db
 
 
 
     //------------------------------------open chat---------------------------------//
+    //when a chat is clicked on from the chat list
+    document.querySelector(`#chat-highlight-${msg.chat_id}`).parentElement.addEventListener('click', openChat());
+    //to store the last x messages of selected chat
+    let lastMessages; // eg. [{msg json including sender (if in a group room)},{},{},{},{}]
+    let chatPanel = document.querySelector('#chat-panel');
 
+    function openChat(e){
+        chatid = e.target.id.split('-')[2];//eg. chat-highlight-123
 
+        //sending the room id to the server to send us back the last messages from the db
+        socket.emit('getlastMessages', chatid);
+
+        //recieving the last messages
+        socket.on('lastMessages', function(lastmsgs) { //only for this client
+            lastMessages = lastmsgs; //list?
+        });
+
+        //appending each message to the chat panel
+        lastMessages.forEach(message => {
+            let msgDiv = document.createElement('div');
+            msgDiv.innerHTML = `<h1>${message.msg}</h1><span>${message.timestamp}</span>`; //and more depending on if the chat is a room or contact
+            msgDiv.id = message.msg_id;
+            chatPanel.append(msgDiv);
+        });
+
+        //setting the global opend_chat variable to the opened chat so as to display any incoming messages in realtime
+        opened_chat = chatid;
+        
+    }
 
 
     //------------------------------------send Message---------------------------------//
